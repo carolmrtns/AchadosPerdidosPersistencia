3a5414211527e7b099635b0102fc835b495f7732
henriquemgomes <henriquemcgomess@gmail.com>
2019-06-27 15:55:15 -0300

    exclusao de metodos nao utilizados

--- src/br/ufsc/ine5605/achadoseperdidos/views/TelaPessoa.java
+++ src/br/ufsc/ine5605/achadoseperdidos/views/TelaPessoa.java
@@ -252,36 +252,6 @@ public class TelaPessoa extends TelaGlobal {
         return painelAluno;
     }
 
-    public void excluirAluno() {
-        System.out.println("----------EXCLUIR ALUNO----------");
-        System.out.println("Matricula: ");
-        int matricula = recebeValorInteiro();
-        ControladorPessoa.getInstancia().excluirAluno(matricula);
-    }
-
-    public void listarAlunos() {
-        System.out.println("----------LISTA DE ALUNOS----------");
-        System.out.println("Nome               Telefone               Matricula");
-        ControladorPessoa.getInstancia().listarAlunos();
-    }
-
-    public void alterarAluno() {
-        System.out.println("----------ALTERAR ALUNO----------");
-        System.out.println("Digite a Matricula do aluno que deseja alterar: ");
-        int matricula = recebeValorInteiro();
-        Aluno aluno = ControladorPessoa.getInstancia().encontrarAlunoPelaMatricula(matricula);
-        this.exibirDadosAluno(aluno.getNomePessoa(), aluno.getTelefonePessoa(), aluno.getMatricula());
-        System.out.println("----------------------------------");
-        System.out.println("Novo Nome: ");
-        String novoNome = recebeValorString();
-        System.out.println("Novo Telefone: ");
-        long novoTelefone = recebeValorLong();
-        System.out.println("Nova Matricula: ");
-        int novaMatricula = recebeValorInteiro();
-        ControladorPessoa.getInstancia().alterarAluno(aluno, novoNome, novoTelefone, novaMatricula);
-        this.exibirDadosAluno(novoNome, novoTelefone, novaMatricula);
-    }
-
     public JPanel telaFuncionario() {
         JPanel painelFuncionario = new JPanel();
         painelFuncionario.setLayout(new GridBagLayout());
@@ -378,36 +348,6 @@ public class TelaPessoa extends TelaGlobal {
 
     }
 
-    public void excluirFuncionario() {
-        System.out.println("----------EXCLUIR FUNCIONARIO----------");
-        System.out.println("Siape: ");
-        int siape = recebeValorInteiro();
-        ControladorPessoa.getInstancia().excluirFuncionario(siape);
-    }
-
-    public void listarFuncionarios() {
-        System.out.println("----------LISTA DE FUNCIONARIOS----------");
-        System.out.println("Nome               Telefone               Siape");
-        ControladorPessoa.getInstancia().listarFuncionarios();
-    }
-
-    public void alterarFuncionario() {
-        System.out.println("----------ALTERAR FUNCIONARIO----------");
-        System.out.println("Digite o Siape do funcionario que deseja alterar: ");
-        int siape = recebeValorInteiro();
-        Funcionario funcionario = ControladorPessoa.getInstancia().encontrarFuncionarioPeloSiape(siape);
-        this.exibirDadosFuncionario(funcionario.getNomePessoa(), funcionario.getTelefonePessoa(), funcionario.getSiape());
-        System.out.println("----------------------------------");
-        System.out.println("Novo Nome: ");
-        String novoNome = recebeValorString();
-        System.out.println("Novo Telefone: ");
-        long novoTelefone = recebeValorLong();
-        System.out.println("Novo Siape: ");
-        int novoSiape = recebeValorInteiro();
-        ControladorPessoa.getInstancia().alterarFuncionario(funcionario, novoNome, novoTelefone, novoSiape);
-        this.exibirDadosFuncionario(novoNome, novoTelefone, novoSiape);
-    }
-
     public JPanel telaVisitante() {
         JPanel painelVisitante = new JPanel();
         painelVisitante.setLayout(new GridBagLayout());
@@ -494,36 +434,6 @@ public class TelaPessoa extends TelaGlobal {
         btnAlterarVisitante.addActionListener(gerenciadorBtnVisitante);
 
         return painelVisitante;
-    }
-
-    public void excluirVisitante() {
-        System.out.println("----------EXCLUIR VISITANTE----------");
-        System.out.println("Cpf: ");
-        long cpf = recebeValorLong();
-        ControladorPessoa.getInstancia().excluirVisitante(cpf);
-    }
-
-    public void listarVisitantes() {
-        System.out.println("----------LISTA DE VISITANTES----------");
-        System.out.println("Nome               Telefone               Cpf");
-        ControladorPessoa.getInstancia().listarVisitantes();
-    }
-
-    public void alterarVisitante() {
-        System.out.println("----------ALTERAR VISITANTE----------");
-        System.out.println("Digite o Cpf do visitante que deseja alterar: ");
-        int cpf = recebeValorInteiro();
-        Visitante visitante = ControladorPessoa.getInstancia().encontrarVisitantePeloCpf(cpf);
-        this.exibirDadosVisitante(visitante.getNomePessoa(), visitante.getTelefonePessoa(), visitante.getCpf());
-        System.out.println("----------------------------------");
-        System.out.println("Novo Nome: ");
-        String novoNome = recebeValorString();
-        System.out.println("Novo Telefone: ");
-        long novoTelefone = recebeValorLong();
-        System.out.println("Novo Cpf: ");
-        int novoCpf = recebeValorInteiro();
-        ControladorPessoa.getInstancia().alterarVisitante(visitante, novoNome, novoTelefone, novoCpf);
-        this.exibirDadosVisitante(novoNome, novoTelefone, novoCpf);
     }
 
     public void exibirDadosAluno(String nome, long telefone, int matricula) {
@@ -623,20 +533,21 @@ public class TelaPessoa extends TelaGlobal {
                         ControladorPessoa.getInstancia().alterarAluno(aluno, nome, telefone, matricula);
                         updateDataAluno();
                         break;
-                    case "3":                                               
+                    case "3":
                         matricula = Integer.parseInt(txtMatricula.getText());
-                        int response = JOptionPane.showConfirmDialog(null, "Você deseja excluir o Aluno matricula: "+ matricula +"?", "Confirmar Exclusão Aluno",
-                        JOptionPane.YES_NO_OPTION, JOptionPane.OK_CANCEL_OPTION);
+                        int response = JOptionPane.showConfirmDialog(null, "Você deseja excluir o Aluno matricula: " + matricula + "?", "Confirmar Exclusão Aluno",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.OK_CANCEL_OPTION);
                         if (response == JOptionPane.NO_OPTION) {
                             updateDataAluno();
                         } else if (response == JOptionPane.YES_OPTION) {
                             ControladorPessoa.getInstancia().excluirAluno(matricula);
                             updateDataAluno();
-                        }                         
+                        }
                         break;
                 }
+
             } catch (PessoaExistenteException ex) {
-                
+
             }
         }
     }
@@ -649,7 +560,7 @@ public class TelaPessoa extends TelaGlobal {
                 String nome;
                 long telefone;
                 int siape;
-                
+
                 switch (ae.getActionCommand()) {
                     case "1":
                         nome = txtNomeFuncionario.getText();
@@ -667,9 +578,9 @@ public class TelaPessoa extends TelaGlobal {
                         updateDataFuncionario();
                         break;
                     case "3":
-                        siape = Integer.parseInt(txtSiape.getText());                        
-                        int response = JOptionPane.showConfirmDialog(null, "Você deseja excluir o Funcionario siape: "+ siape +"?", "Confirmar Exclusão Funcionário",
-                        JOptionPane.YES_NO_OPTION, JOptionPane.OK_CANCEL_OPTION);
+                        siape = Integer.parseInt(txtSiape.getText());
+                        int response = JOptionPane.showConfirmDialog(null, "Você deseja excluir o Funcionario siape: " + siape + "?", "Confirmar Exclusão Funcionário",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.OK_CANCEL_OPTION);
                         if (response == JOptionPane.NO_OPTION) {
                             updateDataFuncionario();
                         } else if (response == JOptionPane.YES_OPTION) {
@@ -679,7 +590,7 @@ public class TelaPessoa extends TelaGlobal {
                         break;
                 }
             } catch (PessoaExistenteException ex) {
-                
+
             }
         }
 
@@ -693,7 +604,7 @@ public class TelaPessoa extends TelaGlobal {
                 String nome;
                 long telefone;
                 int cpf;
-                
+
                 switch (ae.getActionCommand()) {
                     case "1":
                         nome = txtNomeVisitante.getText();
@@ -711,19 +622,19 @@ public class TelaPessoa extends TelaGlobal {
                         updateDataVisitante();
                         break;
                     case "3":
-                        cpf = Integer.parseInt(txtCpf.getText());                        
-                        int response = JOptionPane.showConfirmDialog(null, "Você deseja excluir o Visitante cpf: "+ cpf +"?", "Confirmar Exclusão Visitante",
-                        JOptionPane.YES_NO_OPTION, JOptionPane.OK_CANCEL_OPTION);
+                        cpf = Integer.parseInt(txtCpf.getText());
+                        int response = JOptionPane.showConfirmDialog(null, "Você deseja excluir o Visitante cpf: " + cpf + "?", "Confirmar Exclusão Visitante",
+                                JOptionPane.YES_NO_OPTION, JOptionPane.OK_CANCEL_OPTION);
                         if (response == JOptionPane.NO_OPTION) {
                             updateDataVisitante();
-                        } else if (response == JOptionPane.YES_OPTION) {                            
+                        } else if (response == JOptionPane.YES_OPTION) {
                             ControladorPessoa.getInstancia().excluirVisitante(cpf);
                             updateDataVisitante();
                         }
                         break;
                 }
             } catch (PessoaExistenteException ex) {
-                
+
             }
         }
 
