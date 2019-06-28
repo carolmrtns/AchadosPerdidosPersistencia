5358f35ea714f339c709057ecc65ea89bc7a7452
caroline <caroline@caroline-pc>
2019-06-27 23:54:49 -0300

    Removidos metodos que nao sao mais usados.

--- src/br/ufsc/ine5605/achadoseperdidos/views/TelaGlobal.java
+++ src/br/ufsc/ine5605/achadoseperdidos/views/TelaGlobal.java
@@ -13,48 +13,12 @@ import javax.swing.JOptionPane;
  * @author caroline
  */
 public abstract class TelaGlobal extends JFrame{
-    Scanner teclado;
     
     public TelaGlobal(String nomeJanela){
         super(nomeJanela);
-        teclado = new Scanner(System.in);
     }
     
     public void exibirMensagem(String mensagem){
         JOptionPane.showMessageDialog(null, mensagem);
-    }
-    
-    public String recebeValorString(){
-        return teclado.nextLine();
-    }
-    
-    public int recebeValorInteiro(){
-        int valor = -1;
-        do {
-            try {
-                valor = teclado.nextInt();
-            } catch (Exception e) {
-                System.out.println("Valor invalido! Digite um numero inteiro");
-                valor = -1;
-            } finally {
-                teclado.nextLine();
-            }
-        } while (valor == -1);
-        return valor;
-    }    
-    
-    public long recebeValorLong(){
-        long valor = -1;
-        do {
-            try {
-                valor = teclado.nextLong();
-            } catch (Exception e) {
-                System.out.println("Valor invalido! Digite um valor valido");
-                valor = -1;
-            } finally {
-                teclado.nextLine();
-            }
-        } while (valor == -1);
-        return valor;
     }
 }
